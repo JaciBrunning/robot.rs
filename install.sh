d49789bddc0d1fb423f065e5cf7b089a8a3fecc8
Isaac Turner <spacey_sooty@outlook.com>
2023-09-09 20:31:44 +0800

    install script

--- install.sh
+++ install.sh
@@ -1,16 +1,17 @@
+#!bin/bash
 git clone https://github.com/wpilibsuite/allwpilib.git
 
-mv allwpilib/ntcore wpilib-hal/libs/ntcore
-mv allwpilib/hal wpilib-hal/libs/hal
-mv allwpilib/wpimath wpilib-hal/libs/wpimath
-mv allwpilib/wpinet wpilib-hal/libs/wpinet
-mv allwpilib/wpiutil wpilib-hal/libs/wpiutil
+mv allwpilib\ntcore wpilib-hal\libs\ntcore
+mv allwpilib\hal wpilib-hal\libs\hal
+mv allwpilib\wpimath wpilib-hal\libs\wpimath
+mv allwpilib\wpinet wpilib-hal\libs\wpinet
+mv allwpilib\wpiutil wpilib-hal\libs\wpiutil
 
 rm allwpilib -rf
 
-cp wpilib-hal/libs/hal/src/main/native/include/* wpilib-hal/include -r
-cp wpilib-hal/libs/ntcore/src/main/native/include/* wpilib-hal/include -r
-cp wpilib-hal/libs/wpimath/src/main/native/include/* wpilib-hal/include -r
-cp wpilib-hal/libs/wpinet/src/main/native/include/* wpilib-hal/include -r
-cp wpilib-hal/libs/wpiutil/src/main/native/include/* wpilib-hal/include -r
+cp wpilib-hal\libs\ntcore\src\main\include wpilib-hal\include -r
+cp wpilib-hal\libs\hal\src\main\include wpilib-hal\include -r
+cp wpilib-hal\libs\wpimath\src\main\include wpilib-hal\include -r
+cp wpilib-hal\libs\wpinet\src\main\include wpilib-hal\include -r
+cp wpilib-hal\libs\wpiutil\src\main\include wpilib-hal\include -r
 
