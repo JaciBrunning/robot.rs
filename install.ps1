a6c2db15efd68f10c5eed2c5a25b42af8a473277
Isaac Turner <spacey_sooty@outlook.com>
2023-09-10 15:18:59 +0000

    update windows install

--- install.ps1
+++ install.ps1
@@ -8,9 +8,9 @@ Move-Item allwpilib\wpiutil wpilib-hal\libs\wpiutil
 
 Remove-Item allwpilib -Force -Recurse
 
-Copy-Item wpilib-hal\libs\ntcore\src\main\include wpilib-hal\include
-Copy-Item wpilib-hal\libs\hal\src\main\include wpilib-hal\include
-Copy-Item wpilib-hal\libs\wpimath\src\main\include wpilib-hal\include
-Copy-Item wpilib-hal\libs\wpinet\src\main\include wpilib-hal\include
-Copy-Item wpilib-hal\libs\wpiutil\src\main\include wpilib-hal\include
+Copy-Item wpilib-hal\libs\ntcore\src\main\native\include\* wpilib-hal\include
+Copy-Item wpilib-hal\libs\hal\src\main\native\include\* wpilib-hal\include
+Copy-Item wpilib-hal\libs\wpimath\src\main\native\include\* wpilib-hal\include
+Copy-Item wpilib-hal\libs\wpinet\src\main\native\include\* wpilib-hal\include
+Copy-Item wpilib-hal\libs\wpiutil\src\main\native\include\* wpilib-hal\include
 
