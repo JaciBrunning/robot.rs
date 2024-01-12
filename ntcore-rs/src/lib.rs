d8472998192a858954906157a2eb389ae4f75428
Jaci Brunning <jaci.brunning@gmail.com>
2024-01-12 22:50:16 +1100

    Add ntcore files

--- ntcore-rs/src/lib.rs
+++ ntcore-rs/src/lib.rs
@@ -1,1 +1,14 @@
 pub use robot_rs_ntcore_sys as nt_internal;
+
+pub mod instance;
+pub mod topic;
+pub mod types;
+pub mod entry;
+
+#[macro_export]
+macro_rules! nt {
+  ($path:expr, $value:expr) => { nt!(nt4_rs::instance::NetworkTableInstance::default(), $path, $value) };
+  ($inst:expr, $path:expr, $value:expr) => { $inst.entry($path).set($value) };
+  (read $path:expr) => { nt!(read nt4_rs::instance::NetworkTableInstance::default(), $path) };
+  (read $inst:expr, $path:expr) => { $inst.entry($path).get() };
+}
